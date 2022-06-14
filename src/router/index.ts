51cbabb0f2ebdd9a3f23fff6bb3d3d0c0caf77f3
karuna <karuna@yectra.com>
2022-06-14 11:07:10 +0530

    login page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/login/Index.vue';
 
 Vue.use(VueRouter)
 
@@ -17,7 +18,24 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: { anonymous: true }
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () => import("../views/dashboard/Dashboard.vue"),
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    component: () => import('../views/login/Registration.vue'),
+    meta: { anonymous: true }
+  },
 ]
 
 const router = new VueRouter({
