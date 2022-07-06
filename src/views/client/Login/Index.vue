d5caee5819e0fc27babd1ed652a1325fe535f80b
karuna <karuna@yectra.com>
2022-07-06 10:41:22 +0530

    ClientLogin fix

--- src/views/client/Login/Index.vue
+++ src/views/client/Login/Index.vue
@@ -19,18 +19,18 @@
     </v-col>
     <v-col>
       <v-container fill-height fluid class="d-flex justify-center">
+        <h2 class="mt-10">Feel stuck? We are here to help you!</h2>
         <div>
-          <h2>Feel stuck? We are here to help you!</h2>
           <v-card
             elevation="2"
             outlined
             width="450"
             color="#F8E6C4"
-            class="rounded-lg pl-6 pb-4 mb-8"
+            class="rounded-lg pb-4 mb-8"
           >
-            <v-row class="mb-n8">
+            <v-row>
               <v-col cols="12" md="4" offset-md="4">
-                <v-card-title class="text-h5">Client</v-card-title>
+                <v-card-title class="text-h5 ml-4">Client</v-card-title>
               </v-col>
               <v-col>
                 <v-icon color="#FCB258" class="mt-5 ml-16"
@@ -38,26 +38,50 @@
                 >
               </v-col>
             </v-row>
-            <v-form>
-              <text-input />
-              <password-input />
-              <div class="d-flex justify-center ml-n4">
+            <v-form class="px-8" ref="form" autocomplete="off" @submit="login">
+              <!-- <text-input />
+              <password-input /> -->
+              <v-text-field
+                label="Username"
+                background-color="white"
+                rounded
+                outlined
+                dense
+                :rules="emailRules"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                background-color="white"
+                :rules="passwordRules"
+                :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="() => (value = !value)"
+                :type="value ? 'password' : 'text'"
+                required
+                outlined
+                rounded
+                dense
+              ></v-text-field>
+              <div class="d-flex justify-center">
                 <v-btn
                   depressed
                   rounded
                   color="background-orange"
                   class="white--text text-capitalize"
-                  @click="login"
+                  @click.prevent="login"
+                  :loading="loading"
+                  type="submit"
                   >Login</v-btn
                 >
               </div>
             </v-form>
           </v-card>
           <div class="d-flex justify-center">
-          If you are an admin,
-          <router-link to="/admin/login" class="text-decoration-none"
-            >Click here</router-link
-          ></div>
+            If you are an admin,
+            <router-link to="/admin/login" class="text-decoration-none"
+              >Click here</router-link
+            >
+          </div>
           <div class="d-flex justify-center">
             If you are a coach,
             <router-link to="/coach/login" class="text-decoration-none"
@@ -65,10 +89,10 @@
             >
           </div>
           <div class="d-flex justify-center mt-16">
-          No account?
-          <router-link to="/client/registration" class="text-decoration-none"
-            >Register here</router-link
-          >
+            No account?
+            <router-link to="/client/registration" class="text-decoration-none"
+              >Register here</router-link
+            >
           </div>
         </div>
 
@@ -142,6 +166,16 @@ import PasswordInput from "@/components/controls/PasswordInput.vue";
   components: { TextInput, PasswordInput },
 })
 export default class Login extends Vue {
+  public loading: boolean = false;
+  public value: boolean = true;
+
+  public emailRules: any = [
+    (v: any) => !!v || "E-mail is required",
+    (v: any) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+  ];
+
+  public passwordRules: any = [(v: any) => !!v || "Password is required"];
+
   public login() {
     this.$router.push("home/dashboard");
   }
