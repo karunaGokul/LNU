1150d748939aab5895bee7b26da26c486b73d51b
Logeswaran sugumaran <logeswaran@yectra.com>
2022-07-20 16:07:48 +0530

    Payment page redesigned

--- src/views/client/MainLayout/Payment.vue
+++ src/views/client/MainLayout/Payment.vue
@@ -1,66 +1,78 @@
 <template>
   <div class="pa-8">
     <v-container fluid>
-      <v-simple-table class="mt-10">
-        <template v-slot:default>
-          <thead>
-            <th v-for="item in headers" :key="item" class="background-orange">
-              {{ item }}
-            </th>
-          </thead>
-          <tbody>
-            <tr v-for="item in items" :key="item" class="text-center">
-              <td>{{ item.CounsellingType }}</td>
-              <td>{{ item.Date }}</td>
-              <td>{{ item.Coach }}</td>
-              <td>{{ item.Duration }}</td>
-              <td>{{ item.AmountPaid }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
+      <div class="text-h5 font-weight-bold">Payments</div>
+      <v-card>
+        <v-simple-table class="mt-4">
+          <template v-slot:default>
+            <thead>
+              <th
+                v-for="(header, i) in headers"
+                :key="i"
+                class="background-orange pa-4"
+              >
+                {{ header }}
+              </th>
+            </thead>
+            <tbody>
+              <tr
+                v-for="(item, index) in items"
+                :key="index"
+                class="text-center"
+              >
+                <td>{{ item.CounsellingType }}</td>
+                <td>{{ item.Date }}</td>
+                <td>{{ item.Coach }}</td>
+                <td>{{ item.Duration }}</td>
+                <td>{{ item.AmountPaid }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card>
     </v-container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Inject } from "vue-property-decorator";
-import { PaymentRequestModel, PaymentResponseModel } from "@/model";
+import { Component, Inject } from "vue-property-decorator";
 
+import { PaymentRequestModel, PaymentResponseModel } from "@/model";
 import { IPaymentService } from "@/service";
+
 import BaseComponent from "@/components/base/BaseComponent";
 
 @Component
 export default class Payment extends BaseComponent {
   @Inject("paymentService") paymentService: IPaymentService;
+
   public request: PaymentRequestModel = new PaymentRequestModel();
   public response: PaymentResponseModel = new PaymentResponseModel();
 
   created() {
-    this.payment()
+    this.payment();
   }
 
   public payment() {
     // this.request.ClientId = "id102"
-    console.log(this.request)
-     this.loadingSpinner("show");
-    this.paymentService
-        .payment(this.request)
-        .then((response: Array<PaymentResponseModel>) => {
-          console.log(response);
-           this.loadingSpinner("hide");
-        },
-        (err) => {
-          this.loadingSpinner("hide");
-          if (err.response.status === 400) {
-            // this.snackbarText = err.response.data;
-            // this.snackbar = true;
-          }
+    console.log(this.request);
+    this.loadingSpinner("show");
+    this.paymentService.payment(this.request).then(
+      (response: Array<PaymentResponseModel>) => {
+        console.log(response);
+        this.loadingSpinner("hide");
+      },
+      (err) => {
+        this.loadingSpinner("hide");
+        if (err.response.status === 400) {
+          // this.snackbarText = err.response.data;
+          // this.snackbar = true;
         }
-        );
+      }
+    );
   }
 
-  public headers: any = [
+  public headers: Array<string> = [
     "Counselling Type",
     "Date",
     "Coach",
@@ -68,47 +80,47 @@ export default class Payment extends BaseComponent {
     "Amount Paid",
   ];
 
-  public items: any = [
+  public items: Array<PaymentResponseModel> = [
     {
       CounsellingType: "Foo",
       Date: "10.2.22",
       Coach: "alex",
-      Duration: "25min",
+      Duration: "25 min",
       AmountPaid: "1200",
     },
     {
       CounsellingType: "Bar",
       Date: "15.3.22",
       Coach: "jai",
-      Duration: "30min",
+      Duration: "30 min",
       AmountPaid: "1500",
     },
     {
       CounsellingType: "Buzz",
       Date: "11.4.22",
       Coach: "jhon",
-      Duration: "15min",
+      Duration: "15 min",
       AmountPaid: "500",
     },
     {
       CounsellingType: "Foo",
       Date: "10.2.22",
       Coach: "alex",
-      Duration: "25min",
+      Duration: "25 min",
       AmountPaid: "1200",
     },
     {
       CounsellingType: "Bar",
       Date: "15.3.22",
       Coach: "jai",
-      Duration: "30min",
+      Duration: "30 min",
       AmountPaid: "1500",
     },
     {
       CounsellingType: "Buzz",
       Date: "11.4.22",
       Coach: "jhon",
-      Duration: "15min",
+      Duration: "15 min",
       AmountPaid: "500",
     },
   ];
@@ -116,9 +128,7 @@ export default class Payment extends BaseComponent {
 </script>
 <style>
 tr:nth-of-type(even) {
-  background-color: #fca744;
-  opacity: 0.5;
-  color: white;
+  background-color: #f7a3422e;
 }
 tr:nth-of-type(even):hover {
   background-color: #fca744 !important;
