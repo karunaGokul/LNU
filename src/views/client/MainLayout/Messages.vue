0c1f0c1300ade2f98e6afc453fba31ff39e97219
karuna <karuna@yectra.com>
2022-07-21 11:45:41 +0530

    fix

---
+++ src/views/client/MainLayout/Messages.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid class="pa-8">
+    <v-row>
+      <v-col>
+        <v-select label="New Messages" :items="items" solo></v-select>
+      </v-col>
+      <v-col>
+        <v-select label="New Messages" :items="items" solo></v-select>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Messages extends Vue {
+  public items = ["Foo", "Bar", "Fizz", "Buzz"];
+  
+}
+</script>
