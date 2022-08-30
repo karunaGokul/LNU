f443ca87e7921bc04a5d0ad2374af425c33cd674
pugal bharathi <pugalbharthi61199@gmail.com>
2022-08-30 19:00:50 +0530

    Error fixed

--- src/views/coach/MainLayout/Appointment/component/Calendar.vue
+++ src/views/coach/MainLayout/Appointment/component/Calendar.vue
@@ -166,12 +166,9 @@ export default class Calendar extends BaseComponent {
   public selectedElement: any = null;
   public selectedOpen: boolean = false;
 
-  // public events: Array<any> = [];
   public showAlert: boolean = false;
   public removeAppointment: CancelAppointmentModel =
     new CancelAppointmentModel();
-  public requests: AppoinmentRequestModel = new AppoinmentRequestModel();
-  public response: Array<AppointmentResponseModel> = [];
 
   // public events: Array<EventsModel> = [];
   public typeToLabel: any = {
@@ -180,15 +177,7 @@ export default class Calendar extends BaseComponent {
     day: "Day",
     "4day": "4 Days",
   };
-  public colors: Array<string> = [
-    "blue",
-    "indigo",
-    "deep-purple",
-    "cyan",
-    "green",
-    "orange",
-    "grey darken-1",
-  ];
+
   public names: Array<string> = [
     "Meeting",
     "Holiday",
@@ -276,49 +265,6 @@ export default class Calendar extends BaseComponent {
       });
     // }
   }
-
-  // public getAppointments(status: string, date?: any) {
-  //   if (!date) date = this.$vuehelper.date.format(new Date(), "YYYY-MM-DD");
-
-  //   this.requests.dateRange = date;
-  //   this.requests.status = status;
-  //   this.events = [];
-  //   this.appointmentService
-  //     .getAppointments(this.requests)
-  //     .then((response: Array<AppointmentResponseModel>) => {
-  //       this.response = response;
-
-  //       response.forEach((item) => {
-  //         let event: EventsModel = new EventsModel();
-  //         event.name = item.counselingType.name;
-  //         event.start = this.getDate(
-  //           item.appointmentDate,
-  //           item.appointmentStartTime
-  //         );
-  //         event.end = this.getDate(
-  //           item.appointmentDate,
-  //           item.appointmentEndTime
-  //         );
-  //         event.color =
-  //           this.colors[Math.floor(Math.random() * this.colors.length)];
-  //         event.timed = true;
-  //         event.id = item.id;
-  //         this.events.push(event);
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
-
-  // private getDate(date: string, time: string) {
-  //   let value: Date = new Date(date);
-  //   value.setHours(parseInt(time.split(":")[0]));
-  //   value.setMinutes(parseInt(time.split(":")[1]));
-  //   value.setMilliseconds(parseInt(time.split(":")[2]));
-
-  //   return value;
-  // }
 
   public deleteAppointment(value: boolean) {
     this.showAlert = true;
